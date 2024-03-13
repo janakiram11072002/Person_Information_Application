979ea9087049f96c90b148e7fbe8f1b94fd6c34c
Janakiram G K <76196523+janakiram11072002@users.noreply.github.com>
2024-03-13 06:53:35 +0000

    Created a SpringBoot Project.

---
+++ jhonny/src/main/java/com/person_information/jhonny/JhonnyApplication.java
@@ -0,0 +1,13 @@
+package com.person_information.jhonny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhonnyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JhonnyApplication.class, args);
+	}
+
+}
---
+++ jhonny/src/test/java/com/person_information/jhonny/JhonnyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.person_information.jhonny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JhonnyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
